18e31f4bfab50777dbefcbd59a67e6c20a32e23e
Raian <raian.guimaraes@hotmail.com>
2026-01-08 00:42:11 -0300

    array4 - primos (vlw guanabara)

--- Arrays/ex_array4.py
+++ Arrays/ex_array4.py
@@ -1,9 +1,24 @@
 """
-4. Verificar se um número é primo
+rainz - 07/01/2026
 
-Crie uma função que receba um número e retorne se ele é primo ou não.
+4. Check if a number is prime
 
-Exemplo
-Entrada: 7
-Saída: true
+Create a function that receives a number and returns whether it is prime or not.
+
+Example
+Input: 7
+Output: true
 """
+
+def is_prime(num):
+    divisors = 0
+    for x in range(1, num + 1):
+        if num % x == 0:
+            divisors += 1
+
+    if divisors == 2:
+        print(f"{num} is prime")
+    else:
+        print(f"{num} is not prime")
+
+is_prime(2)
